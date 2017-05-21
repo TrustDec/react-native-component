49edc6cc3ab4a9cab89151105880a7b5dc206ab1
TrustTheBoy <603049583@qq.com>
2017-05-21 20:29:28 +0800

    超出列表后报错，下版修复

--- Example/view/Main.js
+++ Example/view/Main.js
@@ -1,25 +1,21 @@
 'use strict';
-
 import React,{ Component } from 'react';
-import { Image,StyleSheet,LayoutAnimation,Text,TouchableHighlight,PanResponder,View } from 'react-native';
+import ReactNative from 'react-native';
 import Util from './util';
+const {Image,StyleSheet,LayoutAnimation,Text,TouchableHighlight,PanResponder,View} = ReactNative;
 import Icon from 'react-native-vector-icons/Ionicons';
 import IconFA from 'react-native-vector-icons/FontAwesome';
-
-class Sortable extends Component{
+export default class Main extends Component {
   constructor() {
     super();
-    this._width = Util.size.width/3;
+    this._width = Util.size.width/4;
     this.topIndex = 0;
     this.leftIndex = 0;
     this.index = 0;
-    this.finalTopIndex = 0;
-    this.finalLeftIndex = 0;
-    this.finalIndex = 0;
     this.prev_left = 0;
     this.prev_top = 0;
-    this.left = 0;
-    this.top = 0;
+    this.finalTopIndex = 0;
+    this.finalLeftIndex = 0;
     this.animations = {
       duration: 200,
       create: {
@@ -30,10 +26,187 @@ class Sortable extends Component{
         springDamping: 0.7,
       },
     };
-    // last item to be selected as default
-    this.state = {
+    this.state={
       selected: 14,
-      days: [{
+      days:viewbox
+    }
+  }
+  componentWillMount(){
+    this._panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: (evt, gestureState) => {
+        return gestureState.dx!==0;
+      },
+      onMoveShouldSetPanResponder: (evt, gestureState) => true,
+      onPanResponderTerminationRequest: (evt, gestureState) => true,
+      onShouldBlockNativeResponder: (event, gestureState) => true,
+      onPanResponderGrant: (evt, gestureState) => {
+        const {pageX,pageY} = evt.nativeEvent;
+        this.topIndex = Math.floor((pageY-30)/this._width);
+        this.leftIndex = Math.floor((pageX)/this._width);
+        this.index = this.topIndex*4 + this.leftIndex;
+        this.prev_left = this._width * this.leftIndex;
+        this.prev_top = this._width * this.topIndex;
+        this.setState({
+          selected:this.index
+        });
+        let box = this.refs["box" + this.index];
+        box.setNativeProps({
+          style:{
+            opacity:0.7,
+            shadowColor: "#000",
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+            shadowOffset: {
+              height: 0,
+              width: 2
+            }
+          }
+        });
+      },
+      onPanResponderMove:(evt, gestureState)=>{
+        this.left = this.prev_left + gestureState.dx;
+        this.top =  this.prev_top + gestureState.dy;
+        console.log(this.index);
+        let box = this.refs["box" + this.index];
+          box.setNativeProps({
+            style: {top:this.top, left:this.left},
+          });
+        this.endMove(evt, gestureState);
+      },
+      onPanResponderRelease: (evt, gestureState) => this._release(evt, gestureState),
+      onPanResponderTerminate: (evt, gestureState) => this._release(evt, gestureState),
+    });
+  }
+  endMove(evt, gestureState){
+    this.finalTopIndex = Math.floor(this.top / this._width + 0.5);
+    this.finalLeftIndex = Math.floor(this.left / this._width + 0.5);
+    if ((-1 < this.finalTopIndex) && (this.finalTopIndex <4) && (-1 < this.finalLeftIndex) && this.finalLeftIndex < 4) {
+      this.finalIndex = this.finalTopIndex*4 + this.finalLeftIndex;
+      let days = this.state.days;
+      let movedBox = days[this.index];
+      days.splice(this.index, 1);
+      days.splice(this.finalIndex, 0, movedBox);
+      this.setState({
+        days
+      });
+
+      if (this.finalIndex != this.index) {
+        this.index = this.finalIndex;
+        this.setState({
+          selected: this.finalIndex,
+        });
+      }
+
+      LayoutAnimation.configureNext(this.animations);
+    }else {
+      let box = this.refs["box" + this.index];
+      let top = this.topIndex*this._width;
+      let left = this.leftIndex*this._width;
+      LayoutAnimation.configureNext(this.animations);
+    }
+  }
+  _release(){
+    const shadowStyle = {
+      opacity:1,
+      shadowColor: "#000",
+      shadowOpacity: 0,
+      shadowRadius: 0,
+      shadowOffset: {
+        height: 0,
+        width: 0,
+      }
+    };
+    if ((-1 < this.finalTopIndex) && (this.finalTopIndex <4) && (-1 < this.finalLeftIndex) && this.finalLeftIndex < 4) {     
+      let box = this.refs["box" + this.finalIndex];
+      let top = this.finalTopIndex*this._width;
+      let left = this.finalLeftIndex*this._width;
+      if (box) {
+      box.setNativeProps({
+        style: {top,left,...shadowStyle},
+      });
+        
+      }
+      LayoutAnimation.configureNext(this.animations);
+    } else {
+      let box = this.refs["box" + this.index];
+      let top = this.topIndex*this._width;
+      let left = this.leftIndex*this._width;
+      box.setNativeProps({
+        style: {top,left,...shadowStyle},
+      });
+      LayoutAnimation.configureNext(this.animations);
+    }
+  }
+  render(){
+    const boxes = this.state.days.map((item,index)=>{
+      let top = Math.floor(index/4)*this._width;
+      let left = (index%4)*this._width;
+      return(
+        <View ref={"box"+index}
+          key={item.key}
+          {...this._panResponder.panHandlers}
+          style={[styles.touchBox,{top,left}]}
+          underlayColor="#eee"
+        >
+        <View style={styles.boxContainer}>
+          <Text style={styles.boxText}>Day{index+1}</Text>
+          {item.isFA? <IconFA size={item.size} name={item.icon} style={[styles.boxIcon,{color:item.color}]}></IconFA>:
+              <Icon size={item.size} name={item.icon} style={[styles.boxIcon,{color:item.color}]}></Icon>}
+        </View>
+        </View>
+      );
+    });
+    let selectedItem = boxes[this.state.selected];
+    boxes.splice(this.state.selected, 1);
+    boxes.push(selectedItem);
+    return(
+      <View style={styles.touchBoxContainer}>
+        {boxes}
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  container:{
+    flex:1,
+  },
+  itemWrapper:{
+    backgroundColor: '#f3f3f3'
+  },
+  touchBox:{
+    width: Util.size.width/4,
+    height: Util.size.width/4,
+    backgroundColor:"#fff",
+    position:"absolute",
+    left:0,
+    top:0,
+    borderWidth: Util.pixel,
+    borderColor:"#ccc",
+  },
+  touchBoxContainer:{
+    width: Util.size.width,
+    marginTop:30,
+  },
+  boxContainer:{
+    alignItems:"center",
+    justifyContent:"center",
+    width: Util.size.width/4,
+    height:Util.size.width/4,
+  },
+  boxIcon:{
+    position:"relative",
+    top:-10
+  },
+  boxText:{
+    position:"absolute",
+    bottom:15,
+    width:Util.size.width/4,
+    textAlign:"center",
+    left: 0,
+    backgroundColor:"transparent"
+  },
+});
+const viewbox = [{
         key: 0,
         title: "A stopwatch",
         isFA: false,
@@ -153,190 +326,4 @@ class Sortable extends Component{
         size: 50,
         color: "#ec240e",
         hideNav: false,
-      }]
-    }
-  }
-
-  componentWillMount() {
-    this._panResponder = PanResponder.create({
-      onStartShouldSetPanResponder: (evt, gestureState) => {
-        return gestureState.dx!==0 || gestureState.dx!==0;
-      },
-      onMoveShouldSetPanResponder: (evt, gestureState) => true,
-      onPanResponderGrant: (evt, gestureState) => {
-        const {pageX,pageY} = evt.nativeEvent;
-        //30 to be offset
-        this.topIndex = Math.floor((pageY - 30) / this._width);
-        this.leftIndex = Math.floor((pageX) / this._width);
-        this.index = this.topIndex*3 + this.leftIndex;
-        this.prev_left = this._width * this.leftIndex;
-        this.prev_top = this._width * this.topIndex;
-        this.setState({
-          selected: this.index,
-        });
-        let box = this.refs["box" + this.index];
-        box.setNativeProps({
-          style: { 
-            opacity:0.7,
-            shadowColor: "#000",
-            shadowOpacity: 0.3,
-            shadowRadius: 5,
-            shadowOffset: {
-              height: 0,
-              width: 2
-            }
-          },
-        });
-      },
-      onPanResponderMove: (evt, gestureState) => {
-        this.left = this.prev_left + gestureState.dx;
-        this.top =  this.prev_top + gestureState.dy;
-        let box = this.refs["box" + this.index];
-        box.setNativeProps({
-          style: {top:this.top, left:this.left},
-        });
-        this._endMove(evt, gestureState)
-      },
-      onPanResponderTerminationRequest: (evt, gestureState) => true,
-      onPanResponderRelease: (evt, gestureState) => this._release(evt, gestureState),
-      onPanResponderTerminate: (evt, gestureState) => this._release(evt, gestureState),
-      onShouldBlockNativeResponder: (event, gestureState) => true,
-    });
-  }
-
-  _endMove(evt, gestureState) {
-    this.finalTopIndex = Math.floor(this.top / this._width + 0.5);
-    this.finalLeftIndex = Math.floor(this.left / this._width + 0.5);
-    if ((-1 < this.finalTopIndex) && (this.finalTopIndex <5) && (-1 < this.finalLeftIndex) && this.finalLeftIndex < 3) {     
-      this.finalIndex = this.finalTopIndex*3 + this.finalLeftIndex;
-      let days = this.state.days;
-      let movedBox = days[this.index];
-      days.splice(this.index, 1);
-      days.splice(this.finalIndex, 0, movedBox);
-      this.setState({
-        days
-      })
-      
-      if (this.finalIndex != this.index) {
-        this.index = this.finalIndex;
-        this.setState({
-          selected: this.finalIndex,
-        });
-      }
-      LayoutAnimation.configureNext(this.animations);
-    } else {
-      let box = this.refs["box" + this.index];
-      let top = this.topIndex*this._width;
-      let left = this.leftIndex*this._width;
-      LayoutAnimation.configureNext(this.animations);
-    }
-  }
-
-  _release(evt, gestureState) {
-    const shadowStyle = {
-      opacity:1,
-      shadowColor: "#000",
-      shadowOpacity: 0,
-      shadowRadius: 0,
-      shadowOffset: {
-        height: 0,
-        width: 0,
-      }
-    };
-    if ((-1 < this.finalTopIndex) && (this.finalTopIndex <5) && (-1 < this.finalLeftIndex) && this.finalLeftIndex < 3) {     
-      let box = this.refs["box" + this.finalIndex];
-      let top = this.finalTopIndex*this._width;
-      let left = this.finalLeftIndex*this._width;
-      box.setNativeProps({
-        style: {top,left,...shadowStyle},
-      });
-      LayoutAnimation.configureNext(this.animations);
-    } else {
-      console.log(this.topIndex,this.leftIndex)
-      let box = this.refs["box" + this.index];
-      let top = this.topIndex*this._width;
-      let left = this.leftIndex*this._width;
-      box.setNativeProps({
-        style: {top,left,...shadowStyle},
-      });
-      LayoutAnimation.configureNext(this.animations);
-    }
-  }
-
-  render() {
-    const boxes = this.state.days.map((elem, index) => {
-      let top = Math.floor(index/3)*this._width;
-      let left = (index%3)*this._width;
-      return(
-        <View ref={"box"+index} {...this._panResponder.panHandlers} key={elem.key} style={[styles.touchBox,{top,left}]} underlayColor="#eee">
-          <View style={styles.boxContainer}>
-            <Text style={styles.boxText}>Day{index+1}</Text>
-            {elem.isFA? <IconFA size={elem.size} name={elem.icon} style={[styles.boxIcon,{color:elem.color}]}></IconFA>:
-              <Icon size={elem.size} name={elem.icon} style={[styles.boxIcon,{color:elem.color}]}></Icon>}
-          </View>
-        </View>
-      );
-    })
-
-    let selectedItem = boxes[this.state.selected];
-    boxes.splice(this.state.selected, 1);
-    boxes.push(selectedItem);
-
-    return(
-      <View style={styles.touchBoxContainer}>
-        {boxes}
-      </View>
-    );
-  }
-}
-
-export default class extends Component{
-  render() {
-    return(
-      <View>
-        <Sortable/>
-      </View>
-    )
-  }
-}
-
-const styles = StyleSheet.create({
-  container:{
-    flex:1,
-  },
-  itemWrapper:{
-    backgroundColor: '#f3f3f3'
-  },
-  touchBox:{
-    width: Util.size.width/3,
-    height: Util.size.width/3,
-    backgroundColor:"#fff",
-    position:"absolute",
-    left:0,
-    top:0,
-    borderWidth: Util.pixel,
-    borderColor:"#ccc",
-  },
-  touchBoxContainer:{
-    width: Util.size.width,
-    marginTop:30,
-  },
-  boxContainer:{
-    alignItems:"center",
-    justifyContent:"center",
-    width: Util.size.width/3,
-    height:Util.size.width/3,
-  },
-  boxIcon:{
-    position:"relative",
-    top:-10
-  },
-  boxText:{
-    position:"absolute",
-    bottom:15,
-    width:Util.size.width/3,
-    textAlign:"center",
-    left: 0,
-    backgroundColor:"transparent"
-  },
-});
+      }];
